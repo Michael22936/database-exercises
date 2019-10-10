9a495d8b5abe4778864625ea508ce83db9a96fad
Michael McDonnell <Michael.McDonnell067@gmail.com>
2019-10-10 16:58:54 -0500

    add some things to update_exercises

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,13 +1,18 @@
 USE codeup_test_db;
+SELECT * FROM albums;
 
-UPDATE albums AS 'sales times 10' SET sales = (sales*10);
-
-SELECT *;
-
-UPDATE albums AS 'changed date to 1800' SET release_date = 1800 WHERE release_date < 1980;
+UPDATE albums SET sales = (sales*10);
 
 SELECT *;
 
-UPDATE albums AS 'changed Michael Jackson to Peter Jackson' SET artist = 'Peter Jackson' WHERE artist = 'Michael Jackson';
+SELECT * FROM albums WHERE release_date < 1980;
+
+UPDATE albums SET release_date = release_date - 100 WHERE release_date < 1980;
+
+SELECT *;
+
+SELECT * FROM albums WHERE artist = 'Michael Jackson';
+
+UPDATE albums SET artist = 'Peter Jackson' WHERE artist = 'Michael Jackson';
 
 SELECT *;
