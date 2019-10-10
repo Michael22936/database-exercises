908c393a0ddb276ec2c560baf3161bb5d5984cf0
Michael McDonnell <Michael.McDonnell067@gmail.com>
2019-10-10 16:05:41 -0500

    had to add captions

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,12 +1,18 @@
 USE codeup_test_db;
 
+SELECT 'deleted albums after 1991' AS 'albums';
+
 DELETE FROM albums WHERE release_date > 1991;
 
 SELECT *;
 
+SELECT 'delete albums with disco' AS 'albums';
+
 DELETE FROM albums WHERE genre = 'disco';
 
 SELECT *;
+
+SELECT 'delete albums by Whitney Houston' AS 'albums';
 
 DELETE FROM albums WHERE artist = 'Whitney Houston';
 
