d4e35401526e5cd2dfce3f1ca5048359dd36d37a
Michael McDonnell <Michael.McDonnell067@gmail.com>
2019-10-11 13:35:33 -0500

    complete functions_exercises

---
+++ functions_exercises.sql
@@ -0,0 +1,45 @@
+USE employees;
+
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY last_name, first_name DESC;
+
+SELECT concat(first_name,last_name)
+FROM employees
+WHERE last_name LIKE 'E%' ORDER BY emp_no DESC;
+
+SELECT *
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+
+SELECT *
+FROM employees
+WHERE birth_date LIKE '%-12-25';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%';
+
+SELECT *
+FROM employees
+WHERE gender = 'M'
+  AND (first_name = 'Irena'
+    OR first_name = 'Vidya'
+    OR first_name = 'Maya');
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
+
+SELECT DATEDIFF(CURDATE(), hire_date)
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+  AND birth_date LIKE '%-12-25' ORDER BY birth_date, hire_date DESC;
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%' AND NOT last_name LIKE '%qu%';
