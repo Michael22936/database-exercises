e6d1dd77f69bc581ad723bb2226eaa3674834cc5
Michael McDonnell <Michael.McDonnell067@gmail.com>
2019-10-16 16:04:02 -0500

    complete subqueries_exercises.sql

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -24,4 +24,10 @@ WHERE emp_no IN (
     WHERE first_name = 'Aamod'
 );
 
-
+SELECT first_name, last_name
+FROM employees
+WHERE gender = 'F' AND emp_no In(
+    SELECT emp_no
+    FROM dept_manager
+    WHERE to_date > NOW()
+);
